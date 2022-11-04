1f642a2d4855c69c01da8dd734a617e48ba25783
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-11-03 22:04:31 -0500

    obteniedo valores desde la terminal

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const { crearArchivoTabla } = require("./helpers/multiplicar.js");
 
-const base = 4;
+// const base = 4;
 console.clear();
-crearArchivoTabla(base)
+console.log(process.argv);
+
+const [ , ,base = 5] = process.argv;
+const [, nuevaBase = 5] = base.split('=')
+console.log('valor de la base', nuevaBase);
+crearArchivoTabla(nuevaBase)
   .then((resp) => {
     console.log(resp, "creado");
   })
   .catch((error) => console.log("Ocurrió un error", error));
+
